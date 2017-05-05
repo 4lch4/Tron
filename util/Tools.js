9168990efe0ff58332a636b1fc1460286b4579f0
Devin Leaman <dleaman212@gmail.com>
2017-05-05 03:38:32 -0700

    Stop tracking images
    Instead of loading every single gif I use onto GitHub, I'm just going to stop tracking this whole portion.

--- util/Tools.js
+++ util/Tools.js
@@ -118,6 +118,16 @@ class Tools {
         });
     }
 
+    getCommandUsage(callback) {
+        let queryString = "SELECT * FROM commands;";
+
+        connection.query(queryString, (err, res, fields) => {
+            if (err) throw err;
+
+            callback(res);
+        });
+    };
+
     getExhentaiCookies() {
         return exhentaiCookies;
     }
