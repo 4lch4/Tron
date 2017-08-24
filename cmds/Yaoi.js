81846bb95fe802e1aba30697d7e8574239ffbac8
Devin Leaman <dleaman212@gmail.com>
2017-08-23 19:00:47 -0500

    Updated Yaoi cmd to pull all images from Zenny's tumblr
    Zenny created a tumblr at https://lady-zenora.tumblr.com/ for posting quality yaoi content to be displayed with Tron. Apparently the previous pages were half-assed in their content and it wasn't appealing.

--- cmds/Yaoi.js
+++ cmds/Yaoi.js
@@ -14,14 +14,7 @@ const client = tumblr.createClient({
 const Tools = require('../util/Tools');
 const tools = new Tools();
 
-const blogUrls = [
-    "yaoi-all-my-life.tumblr.com",
-    "yaoiisaneed.tumblr.com",
-    "fuckyeahexplicityaoi.tumblr.com",
-    "secretyaoilover.tumblr.com",
-    "yummy-yaoi.tumblr.com",
-    "yaoi-gifs.tumblr.com"
-];
+const blogUrl = "lady-zenora.tumblr.com";
 
 class Yaoi {
     constructor(options) {
@@ -30,12 +23,10 @@ class Yaoi {
 
     getYaoiPhoto() {
         return new Promise((resolve, reject) => {
-            this.pickRandomBlog().then((url) => {
-                this.pickRandomPost(url).then((post) => {
-                    let random = tools.getRandom(0, post.photos.length);
-                    
-                    resolve(post.photos[random].original_size.url);
-                });
+            this.pickRandomPost(blogUrl).then((post) => {
+                let random = tools.getRandom(0, post.photos.length);
+
+                resolve(post.photos[random].original_size.url);
             });
         });
     }
