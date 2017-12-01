71d3584f5ef1c0b67a657244a3e75a7777089bce
Devin Leaman <dleaman212@gmail.com>
2017-11-30 22:32:09 -0600

    Added Avatar command

---
+++ cmds/feature/Avatar.js
@@ -0,0 +1,27 @@
+const { Command } = require('discord.js-commando')
+
+class Avatar extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'avatar',
+      group: 'features',
+      memberName: 'avatar',
+      throttling: { usages: 1, duration: 5 },
+      description: 'Returns a link to the tagged users avatar.',
+      details: 'Returns a high quality (if available) version of the tagged users avatar.',
+      examples: ['+avatar @Alcha#2621'],
+      args: [{
+        key: 'user',
+        label: 'User',
+        prompt: 'Which user did you want the avatar of?',
+        type: 'user'
+      }]
+    })
+  }
+
+  async run (msg, { user }) {
+    msg.channel.send(user.displayAvatarURL({ size: 2048 }))
+  }
+}
+
+module.exports = Avatar
