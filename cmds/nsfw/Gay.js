bc8b084f52a9a2918224a229f96f63a1828e5e4b
Devin Leaman <dleaman212@gmail.com>
2018-02-21 15:32:08 -0600

    Updated gay aliases and .gitignore

--- cmds/nsfw/Gay.js
+++ cmds/nsfw/Gay.js
@@ -20,6 +20,7 @@ class Gay extends Command {
       name: 'gay',
       group: 'nsfw',
       memberName: 'gay',
+      aliases: ['dick', 'dicks', 'cock', 'cocks', 'penis', 'penises'],
       throttling: { usages: 1, duration: 10 },
       description: 'Returns a random gay image or gif from a variety of subreddits and tumblr pages.',
       examples: ['+gay'],
