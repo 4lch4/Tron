73e0ebee1e7d5117011bb72b2b0770ea69308f1e
Devin Leaman <dleaman212@gmail.com>
2018-03-02 19:13:36 -0600

    Added count argument to boobs command
    Needs to be added to the other NSFW commands now.

--- cmds/nsfw/Boobs.js
+++ cmds/nsfw/Boobs.js
@@ -22,11 +22,17 @@ class Boobs extends Command {
       throttling: { usages: 1, duration: 10 },
       description: 'Returns a random boob image or gif from a variety of subreddits.',
       examples: ['+boobs'],
+      args: [{
+        key: 'count',
+        type: 'integer',
+        prompt: '',
+        default: 1
+      }],
       nsfw: true
     })
   }
 
-  run (msg, args) {
+  run (msg, { count }) {
     reddit.getRandomTopPost(boobSubs, 'day', 50).then(post => {
       msg.channel.send(post)
     })
