87f562cda95016c73fee8cfbd1d74544f2c0e3db
Devin Leaman <dleaman212@gmail.com>
2018-03-05 22:38:22 -0600

    Cleaned up birthday command

--- cmds/features/Birthdays.js
+++ cmds/features/Birthdays.js
@@ -1,16 +1,6 @@
 const BaseCmd = require('../BaseCmd')
 const Birthday = require('../util/Birthday')
 
-/**
- *
- * @param {string[]} args
- */
-const cleanArs = args => {
-  let newArgs = []
-  args.forEach(val => newArgs.push(val.toLowerCase()))
-  return newArgs
-}
-
 class Birthdays extends BaseCmd {
   constructor (client) {
     super(client, {
@@ -31,43 +21,58 @@ class Birthdays extends BaseCmd {
       args = cleanArs(args)
       switch (args[0]) {
         case 'add':
-          if (await birthday.stored()) {
-            // Users birthday has already been stored, use update
-            return msg.reply('your birthday is already stored. Please use the `update` argument instead of `add`.')
-          } else {
-            const info = await birthday.getInfo(msg)
-            const stored = await birthday.store(info)
-
-            if (stored) msg.reply('your birthday has successfully been stored!')
-            else msg.reply('there was an error saving your birthday info, please contact `+support`.')
-          }
-          break
+          return addBirthday(birthday, msg)
 
         case 'list':
-          birthday.getServerBdays(msg, this.client).then(bdays => {
-            let content = ['Here is a list of server members who have their birthday added and set to public:\n```']
-
-            bdays.forEach(bday => {
-              content.push(`- ${bday.username} on ${bday.date}`)
-            })
-
-            content.push('```')
-
-            msg.channel.send(content.join('\n'))
-          })
-          break
+          return listBirthdays(birthday, msg, this.client)
 
         case 'update':
           return msg.reply('this is currently being developed and is not available to the public.')
 
         default:
-
-          break
+          return msg.reply('now how in the sam-hell did you get this to happen?\nContact Alcha using the `+support` command.')
       }
     } else {
-      msg.reply('working.')
+      listBirthdays(birthday, msg, this.client)
     }
   }
 }
 
 module.exports = Birthdays
+
+const addBirthday = async (birthday, msg) => {
+  if (await birthday.stored()) {
+    // Users birthday has already been stored, use update
+    return msg.reply('your birthday is already stored. Please use the `update` argument instead of `add`.')
+  } else {
+    const info = await birthday.getInfo(msg)
+    const stored = await birthday.store(info)
+
+    if (stored) msg.reply('your birthday has successfully been stored!')
+    else msg.reply('there was an error saving your birthday info, please contact `+support`.')
+  }
+}
+
+const listBirthdays = (birthday, msg, client) => {
+  birthday.getServerBdays(msg, client).then(bdays => {
+    let content = ['Here is a list of server members who have their birthday added and set to public:\n```']
+
+    bdays.forEach(bday => {
+      content.push(`- ${bday.username} on ${bday.date}`)
+    })
+
+    content.push('```')
+
+    msg.channel.send(content.join('\n'))
+  })
+}
+
+/**
+ *
+ * @param {string[]} args
+ */
+const cleanArs = args => {
+  let newArgs = []
+  args.forEach(val => newArgs.push(val.toLowerCase()))
+  return newArgs
+}
