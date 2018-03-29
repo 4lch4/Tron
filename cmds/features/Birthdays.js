efa72fdcc73c8394e93e3cc71fd97e7bf5820ded
Devin Leaman <dleaman212@gmail.com>
2018-03-29 04:26:32 -0500

    Beginning of major birthday overhaul
    The Birthday command will be able to support four arguments, add, list, update, delete. So far, the add argument is complete and the others are in the works to be committed shortly.

--- cmds/features/Birthdays.js
+++ cmds/features/Birthdays.js
@@ -1,3 +1,4 @@
+const { Message, Client } = require('discord.js') // Used for JSDocs
 const BaseCmd = require('../BaseCmd')
 const Birthday = require('../util/Birthday')
 
@@ -15,44 +16,92 @@ class Birthdays extends BaseCmd {
   }
 
   async run (msg, args) {
-    const birthday = new Birthday(msg.author.id)
+    args = this.cleanArgs(args)
+    const op = args[0]
+    const user = getUserIdFromStr(args[1])
 
-    if (args.length > 0) {
-      args = cleanArs(args)
-      switch (args[0]) {
+    if (isValidOperation(op)) {
+      switch (op) {
         case 'add':
+          let birthday
+          if (user === undefined) birthday = new Birthday(msg.author.id)
+          else birthday = new Birthday(user)
+
           return addBirthday(birthday, msg)
 
         case 'list':
           return listBirthdays(birthday, msg, this.client)
 
         case 'update':
-          return msg.reply('this is currently being developed and is not available to the public.')
+          const info = await birthday.getInfo(msg)
+          birthday.updateBirthday(info).then(res => {
+            console.log(res)
+            msg.reply('your birthday has been updated!')
+          })
+          break
 
         default:
-          return msg.reply('now how in the sam-hell did you get this to happen?\nContact Alcha using the `+support` command.')
+          return msg.reply('now, how in the sam hill did you get this to happen?\nPlease contact Alcha using the `+support` command.')
       }
-    } else {
-      listBirthdays(birthday, msg, this.client)
-    }
+    } else return msg.reply('Please provide a valid value (add, list, update, delete).')
   }
 }
 
 module.exports = Birthdays
 
+const isValidOperation = val => {
+  switch (val) {
+    case 'add': return true
+    case 'list': return true
+    case 'update': return true
+    case 'delete': return true
+    default:
+      return false
+  }
+}
+
+/**
+ * Gets a user id from the provided string. Usually the content of a mention so
+ * something like <@397971962737197057> for example.
+ *
+ * @param {string} str The string containing the user id
+ */
+const getUserIdFromStr = str => {
+  if (str === undefined) return undefined
+
+  const start = str.search(/[0-9]/)
+  const end = str.indexOf('>')
+  return str.substring(start, end)
+}
+
+/**
+ *
+ * @param {Birthday} birthday
+ * @param {Message} msg
+ *
+ * @returns {Promise<Message>}
+ */
 const addBirthday = async (birthday, msg) => {
   if (await birthday.stored()) {
-    // Users birthday has already been stored, use update
-    return msg.reply('your birthday is already stored. Please use the `update` argument instead of `add`.')
+    // Users birthday has already been stored
+    return msg.reply('this birthday has already been stored. If you wish to add a birthday for someone else, mention them like so: `+bdays add @User#1234`')
   } else {
     const info = await birthday.getInfo(msg)
     const stored = await birthday.store(info)
 
-    if (stored) msg.reply('your birthday has successfully been stored!')
-    else msg.reply('there was an error saving your birthday info, please contact `+support`.')
+    if (stored) msg.reply('this birthday has successfully been stored!')
+    else msg.reply('there was an error saving this birthday info, please contact `+support`.')
   }
 }
 
+/**
+ *
+ * @param {Birthday} birthday
+ * @param {Message} msg
+ * @param {Client} client
+ *
+ * @returns {Promise<Message>}
+ */
 const listBirthdays = (birthday, msg, client) => {
   birthday.getServerBdays(msg, client).then(bdays => {
     let content = ['Here is a list of server members who have their birthday added and set to public:\n```']
@@ -65,14 +114,4 @@ const listBirthdays = (birthday, msg, client) => {
 
     msg.channel.send(content.join('\n'))
   })
-}
-
-/**
- *
- * @param {string[]} args
- */
-const cleanArs = args => {
-  let newArgs = []
-  args.forEach(val => newArgs.push(val.toLowerCase()))
-  return newArgs
 }
--- cmds/util/Birthday.js
+++ cmds/util/Birthday.js
@@ -1,3 +1,4 @@
+const { Message } = require('discord.js') // Used for JSDocs
 const config = require('../../util/config.json')
 const mongoose = require('mongoose')
 const connection = mongoose.createConnection(config.mongoUrl)
@@ -7,16 +8,38 @@ const BirthdaySchema = new mongoose.Schema({
   privateDate: Boolean
 })
 
-const BirthdayModel = connection.model('Birthday', BirthdaySchema, 'birthdays')
-
 /**
- * @typedef {Object} Info
+ * @typedef {Object} Bday
  * @prop {string} _id
  * @prop {string} date
  * @prop {boolean} privateDate
  */
 
+const BirthdayModel = connection.model('Birthday', BirthdaySchema, 'birthdays')
+
+const baseCollector = msg => {
+  return msg.channel.createMessageCollector(m => {
+    return m.channel.id === msg.channel.id && m.author.id === msg.author.id
+  }, { time: 25000 })
+}
+
+/**
+ * The birthday helper class for managing birthdays and the database entries
+ * associated with them.
+ */
 class Birthday {
+  /**
+   * The default constructor for the Birthday class. It accepts three
+   * parameters, two of which are optional as they may not be known at the time
+   * of instantiating the class. The user id is mandatory as it's used for
+   * nearly every database call and we can't chance it being undefined/null.
+   *
+   * @param {string} userId The user id of the user we're working with
+   * @param {string} date The date of the users birthday if known
+   * @param {boolean} privateDate Whether or not their birthday should be private
+   *
+   * @constructor
+   */
   constructor (userId, date = undefined, privateDate = true) {
     this.user = userId
     this.date = date
@@ -24,8 +47,12 @@ class Birthday {
   }
 
   /**
-   * @returns {Promise<string>}
-  */
+   * Returns true or false depending on if the current birthday model has been
+   * stored in the database yet. The database is queried for the user that was
+   * provided when creating an instance of the Birthday class.
+   *
+   * @returns {Promise<boolean>}
+   */
   stored () {
     return new Promise((resolve, reject) => {
       BirthdayModel.findById(this.user, (err, res) => {
@@ -36,9 +63,27 @@ class Birthday {
     })
   }
 
-  store (object = {_id: this.user, date: this.date, privateDate: this.privateDate}) {
+  updateBirthday (info) {
     return new Promise((resolve, reject) => {
-      const newBday = new BirthdayModel(object)
+      BirthdayModel.findByIdAndUpdate(this.user, { date: info.date, privateDate: info.privateDate }, (err, res) => {
+        if (err) reject(err)
+        else resolve(res)
+      })
+    })
+  }
+
+  /**
+   * If a birthday object is provided, it is stored in the birthdays collection
+   * and returns true if it is successful. If no object is provided, it defaults
+   * to the values provided when creating an instance of the Birthday class.
+   *
+   * @param {Bday} [bday] The birthday to be stored (defaults to values provided when creating the class)
+   *
+   * @return {Promise<boolean>}
+   */
+  store (bday = {_id: this.user, date: this.date, privateDate: this.privateDate}) {
+    return new Promise((resolve, reject) => {
+      const newBday = new BirthdayModel(bday)
       newBday.save(err => {
         if (err) reject(err)
         else resolve(true)
@@ -47,10 +92,12 @@ class Birthday {
   }
 
   /**
-   * Gets the required info to be stored for a given birthday and returns it as an object.
+   * Gets the required info to be stored for a given birthday and returns it as
+   * an object.
    *
-   * @param {*} msg
-   * @returns {Promise<Info>}
+   * @param {Message} msg
+   *
+   * @returns {Promise<Bday>}
    */
   async getInfo (msg) {
     return Promise.resolve({
@@ -60,48 +107,118 @@ class Birthday {
     })
   }
 
+  async getOtherInfo (msg) {
+    return Promise.resolve({
+      _id: await this.getUserId(msg),
+      date: await this.getOtherDate(msg),
+      privateDate: await this.getOtherPrivacy(msg)
+    })
+  }
+
+  /**
+   * Gets a list of all public birthdays on the server where the provided
+   * message was sent.
+   *
+   * @param {Message} msg
+   */
   async getServerBdays (msg) {
     return new Promise((resolve, reject) => {
       BirthdayModel.find({privateDate: false}, (err, res) => {
         if (err) reject(err)
         else {
-          let usernames = []
+          let bdays = []
 
           res.forEach(val => {
             let member = msg.guild.members.get(val._id)
-            if (member !== undefined) usernames.push({username: member.user.username, date: [val.date.slice(0, 2), '/', val.date.slice(2)].join('')})
+            if (member !== undefined) bdays.push({username: member.user.username, date: this.formatDate(val.date)})
           })
 
-          resolve(usernames)
+          resolve(bdays)
         }
       })
     })
   }
 
-  getDate (msg) {
+  async getUserId (msg) {
     return new Promise((resolve, reject) => {
-      const collector = msg.channel.createMessageCollector(m => {
-        return m.channel.id === msg.channel.id && m.author.id === msg.author.id
-      }, { time: 25000 })
+      const collector = baseCollector(msg)
 
-      msg.reply('which date is your birthday? (MMDD e.g. August 25 = 0825)')
+      msg.reply('which user do you want to store a birthday for? Please reply with a mention of the user. (e.g. _@Alcha#2625_)')
 
       collector.on('collect', m => {
-        if (m.content.match(/[0-9]{4}/)) {
+        if (m.mentions.users.size === 1) {
           collector.stop()
-          resolve(m.content)
-        } else m.reply('date must be in MMDD format.')
+          resolve(m.mentions.users.first().id)
+        } else m.reply('please mention at least one and only one, user.')
       })
     })
   }
 
-  getPrivacy (msg) {
+  verifyNewUser (msg) {
     return new Promise((resolve, reject) => {
-      const collector = msg.channel.createMessageCollector(m => {
-        return m.channel.id === msg.channel.id && m.author.id === msg.author.id
-      }, { time: 25000 })
+      const collector = baseCollector(msg)
 
-      msg.reply('would you like your birthday to remain private?')
+      msg.reply('your birthday has already been stored. Would you like to add the birthday of another user? (**Y**/**N**)')
+
+      collector.on('collect', m => {
+        if (m.content.length === 1) {
+          switch (m.content.toLowerCase()) {
+            case 'y':
+              collector.stop()
+              resolve(true)
+              break
+
+            case 'n':
+              collector.stop()
+              resolve(false)
+              break
+
+            default:
+              m.reply('please reply with the letter **Y** or **N**.')
+              break
+          }
+        } else m.reply('please reply with the letter **Y** or **N**.')
+      })
+    })
+  }
+
+  /**
+   * Gets the date of a users birthday using a message collector on the provided
+   * message. If one is provided, it is returned via a Promise as a String.
+   *
+   * @param {Message} msg
+   *
+   * @returns {Promise<string>}
+   */
+  getOtherDate (msg) {
+    return new Promise((resolve, reject) => {
+      const collector = baseCollector(msg)
+
+      msg.reply('which date is their birthday? (MMDD e.g. August 25 = **0825**)')
+
+      collector.on('collect', m => {
+        if (m.content.length === 4 && m.content.match(/[0-9]{4}/)) {
+          collector.stop()
+          resolve(m.content)
+        } else m.reply('date must be in MMDD format (e.g. August 25 = **0825**).')
+      })
+    })
+  }
+
+  /**
+   * Gets the privacy preference from the user using a message collector on the
+   * provided message object. If it is provided, true or false is returned based
+   * on their reply `(yes = true, no = false)`.
+   *
+   * @param {Message} msg
+   *
+   * @returns {Promise<boolean>}
+   */
+  getOtherPrivacy (msg) {
+    return new Promise((resolve, reject) => {
+      const collector = baseCollector(msg)
+
+      msg.reply('would you like their birthday to remain private?')
 
       collector.on('collect', m => {
         let content = m.content.toLowerCase()
@@ -114,6 +231,73 @@ class Birthday {
         } else m.reply('must provide yes or no.')
       })
     })
+  }
+
+  /**
+   * Gets the date of a users birthday using a message collector on the provided
+   * message. If one is provided, it is returned via a Promise as a String.
+   *
+   * @param {Message} msg
+   *
+   * @returns {Promise<string>}
+   */
+  getDate (msg) {
+    return new Promise((resolve, reject) => {
+      const collector = baseCollector(msg)
+
+      msg.reply('which date is the birthday? (MMDD e.g. August 25 = **0825**)')
+
+      collector.on('collect', m => {
+        if (m.content.length === 4 && m.content.match(/[0-1]?[0-9][0-3][0-9]/)) {
+          collector.stop()
+          resolve(m.content)
+        } else m.reply('date must be in MMDD format (e.g. August 25 = **0825**).')
+      })
+    })
+  }
+
+  /**
+   * Gets the privacy preference from the user using a message collector on the
+   * provided message object. If it is provided, true or false is returned based
+   * on their reply `(yes = true, no = false)`.
+   *
+   * @param {Message} msg
+   *
+   * @returns {Promise<boolean>}
+   */
+  getPrivacy (msg) {
+    return new Promise((resolve, reject) => {
+      const collector = baseCollector(msg)
+
+      msg.reply('would you like the birthday to remain private?')
+
+      collector.on('collect', m => {
+        let content = m.content.toLowerCase()
+        if (content === 'yes') {
+          collector.stop()
+          resolve(true)
+        } else if (content === 'no') {
+          collector.stop()
+          resolve(false)
+        } else m.reply('must provide yes or no.')
+      })
+    })
+  }
+
+  /**
+   * "Formats" the given 4 digit date by adding a / between them. For example,
+   * passing in the digits "0825" will return "08/25" for better reading.
+   *
+   * @param {string} date The date to be formatted
+   */
+  formatDate (date) {
+    /* let output = []
+    output.push(date[0])
+    output.push(date[1])
+    output.push('/')
+    output.push(date[2])
+    output.push(date[3]) */
+    return [date.slice(0, 2), '/', date.slice(2)].join('')
   }
 }
 
