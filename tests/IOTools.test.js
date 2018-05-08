90fb85f7acca0592dbda32fa7778c4140a0e88ee
Devin Leaman <dleaman212@gmail.com>
2018-05-08 06:30:44 -0500

    Quick fix for Travis-CI testing

--- tests/IOTools.test.js
+++ tests/IOTools.test.js
@@ -2,7 +2,7 @@ const ioTools = new (require('../util/IOTools'))()
 const path = require('path')
 
 test('The getImagePath(path) function returns a path to the correct directory.', () => {
-  expect(ioTools.getImagePath(path.join('alcha', 'Jerry-1.gif'))).toEqual('images\\alcha\\Jerry-1.gif')
+  expect(ioTools.getImagePath(path.join('alcha', 'Jerry-1.gif'))).toEqual(path.join('images', 'alcha', 'Jerry-1.gif'))
   expect(ioTools.getImagePath('null')).toBe('Invalid file path provided.')
   expect(ioTools.getImagePath('undefined')).toHaveLength(27)
 })
