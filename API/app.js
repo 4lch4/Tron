16b2ae3b866c963f908b334937c6ab68f2e61133
Devin Leaman <dleaman212@gmail.com>
2019-03-25 13:53:27 -0500

    Minor update to app.js
    Updated the port number and added some regions for VSCode.

--- API/app.js
+++ API/app.js
@@ -1,5 +1,5 @@
 const bodyParser = require('body-parser')
-const port = process.env.PORT || 3000
+const port = 3000
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
@@ -12,19 +12,29 @@ const endpoints = {
 }
 
 module.exports = client => {
+  // #region Middleware Initiators
   app.use(bodyParser.json())
   app.use(morgan('tiny'))
+  // #endregion Middleware Initiators
 
+  // #region Handles empty GET request
   app.get('/api/v1', (req, res) => noFields(res, endpoints))
+  // #endregion Handles empty GET request
 
+  // #region Count Endpoint
   app.get('/api/v1/count', (req, res) => require('./routes/count')(req, res, client))
   app.post('/api/v1/count', (req, res) => require('./routes/count')(req, res, client))
+  // #endregion Count Endpoint
 
+  // #region Health Endpoint
   app.get('/api/v1/health', (req, res) => require('./routes/health')(req, res, client))
   app.post('/api/v1/health', (req, res) => require('./routes/health')(req, res, client))
+  // #endregion Health Endpoint
 
+  // #region Client Endpoint
   app.get('/api/v1/client', (req, res) => require('./routes/client')(req, res, client))
   app.post('/api/v1/client', (req, res) => require('./routes/client')(req, res, client))
+  // #endregion Client Endpoint
 
   app.listen(port)
   console.log(`Express listening on port ${port}...`)
